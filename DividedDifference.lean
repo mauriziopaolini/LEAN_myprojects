61c876bf2918b5b6c472184a5800ef9f7c855e48
Maurizio Paolini <maurizio.paolini@unicatt.it>
2026-01-08 10:19:30 +0100

    use a bit of standard

--- DividedDifference.lean
+++ DividedDifference.lean
@@ -1,3 +1,7 @@
+-- This module serves as the root of the `DividedDifference` library.
+-- Import modules here that should be built as part of the library.
+import DividedDifference.Basic
+
 import Mathlib.Analysis.Calculus.LocalExtr.Rolle
 import Mathlib.Analysis.Calculus.IteratedDeriv.Defs
 import Mathlib.Analysis.Calculus.ContDiff.Basic

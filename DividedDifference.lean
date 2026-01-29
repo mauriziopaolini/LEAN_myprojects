ab6a0e06cb13935fa223f6d32aaabcdc8a6babdf
Maurizio Paolini <maurizio.paolini@unicatt.it>
2026-01-29 09:49:35 +0100

    isolate Harmonic and polynomial stuff

--- DividedDifference.lean
+++ DividedDifference.lean
@@ -6,6 +6,7 @@ import Mathlib.Analysis.Calculus.IteratedDeriv.Defs
 import Mathlib.Analysis.Calculus.ContDiff.Basic
 import Mathlib.LinearAlgebra.Lagrange
 import DividedDifference.HigherOrderRolle
+import DividedDifference.Polynomials
 
 import Batteries.Tactic.GeneralizeProofs
 
@@ -122,242 +123,8 @@ Main result: order n divided difference equals the nth derivative of f divided
 by n!
 -/
 
-
-namespace Harmonic.GeneralizeProofs
--- Harmonic `generalize_proofs` tactic
-
-open Lean Meta Elab Parser.Tactic Elab.Tactic Batteries.Tactic.GeneralizeProofs
-def mkLambdaFVarsUsedOnly' (fvars : Array Expr) (e : Expr) : MetaM (Array Expr × Expr) := do
-  let mut e := e
-  let mut fvars' : List Expr := []
-  for i' in [0:fvars.size] do
-    let fvar := fvars[fvars.size - i' - 1]!
-    e ← mkLambdaFVars #[fvar] e (usedOnly := false) (usedLetOnly := false)
-    match e with
-    | .letE _ _ v b _ => e := b.instantiate1 v
-    | .lam _ _ _b _ => fvars' := fvar :: fvars'
-    | _ => unreachable!
-  return (fvars'.toArray, e)
-
-partial def abstractProofs' (e : Expr) (ty? : Option Expr) : MAbs Expr := do
-  if (← read).depth ≤ (← read).config.maxDepth then MAbs.withRecurse <| visit (← instantiateMVars e) ty?
-  else return e
-where
-  visit (e : Expr) (ty? : Option Expr) : MAbs Expr := do
-    if (← read).config.debug then
-      if let some ty := ty? then
-        unless ← isDefEq (← inferType e) ty do
-          throwError "visit: type of{indentD e}\nis not{indentD ty}"
-    if e.isAtomic then
-      return e
-    else
-      checkCache (e, ty?) fun _ ↦ do
-        if ← isProof e then
-          visitProof e ty?
-        else
-          match e with
-          | .forallE n t b i =>
-            withLocalDecl n i (← visit t none) fun x ↦ MAbs.withLocal x do
-              mkForallFVars #[x] (← visit (b.instantiate1 x) none) (usedOnly := false) (usedLetOnly := false)
-          | .lam n t b i => do
-            withLocalDecl n i (← visit t none) fun x ↦ MAbs.withLocal x do
-              let ty'? ←
-                if let some ty := ty? then
-                  let .forallE _ _ tyB _ ← pure ty
-                    | throwError "Expecting forall in abstractProofs .lam"
-                  pure <| some <| tyB.instantiate1 x
-                else
-                  pure none
-              mkLambdaFVars #[x] (← visit (b.instantiate1 x) ty'?) (usedOnly := false) (usedLetOnly := false)
-          | .letE n t v b _ =>
-            let t' ← visit t none
-            withLetDecl n t' (← visit v t') fun x ↦ MAbs.withLocal x do
-              mkLetFVars #[x] (← visit (b.instantiate1 x) ty?) (usedLetOnly := false)
-          | .app .. =>
-            e.withApp fun f args ↦ do
-              let f' ← visit f none
-              let argTys ← appArgExpectedTypes f' args ty?
-              let mut args' := #[]
-              for arg in args, argTy in argTys do
-                args' := args'.push <| ← visit arg argTy
-              return mkAppN f' args'
-          | .mdata _ b  => return e.updateMData! (← visit b ty?)
-          | .proj _ _ b => return e.updateProj! (← visit b none)
-          | _           => unreachable!
-  visitProof (e : Expr) (ty? : Option Expr) : MAbs Expr := do
-    let eOrig := e
-    let fvars := (← read).fvars
-    let e := e.withApp' fun f args => f.beta args
-    if e.withApp' fun f args => f.isAtomic && args.all fvars.contains then return e
-    let e ←
-      if let some ty := ty? then
-        if (← read).config.debug then
-          unless ← isDefEq ty (← inferType e) do
-            throwError m!"visitProof: incorrectly propagated type{indentD ty}\nfor{indentD e}"
-        mkExpectedTypeHint e ty
-      else pure e
-    if (← read).config.debug then
-      unless ← Lean.MetavarContext.isWellFormed (← getLCtx) e do
-        throwError m!"visitProof: proof{indentD e}\nis not well-formed in the current context\n\
-          fvars: {fvars}"
-    let (fvars', pf) ← mkLambdaFVarsUsedOnly' fvars e
-    if !(← read).config.abstract && !fvars'.isEmpty then
-      return eOrig
-    if (← read).config.debug then
-      unless ← Lean.MetavarContext.isWellFormed (← read).initLCtx pf do
-        throwError m!"visitProof: proof{indentD pf}\nis not well-formed in the initial context\n\
-          fvars: {fvars}\n{(← mkFreshExprMVar none).mvarId!}"
-    let pfTy ← instantiateMVars (← inferType pf)
-    let pfTy ← abstractProofs' pfTy none
-    if let some pf' ← MAbs.findProof? pfTy then
-      return mkAppN pf' fvars'
-    MAbs.insertProof pfTy pf
-    return mkAppN pf fvars'
-partial def withGeneralizedProofs' {α : Type} [Inhabited α] (e : Expr) (ty? : Option Expr)
-    (k : Array Expr → Array Expr → Expr → MGen α) :
-    MGen α := do
-  let propToFVar := (← get).propToFVar
-  let (e, generalizations) ← MGen.runMAbs <| abstractProofs' e ty?
-  let rec
-    go [Inhabited α] (i : Nat) (fvars pfs : Array Expr)
-        (proofToFVar propToFVar : ExprMap Expr) : MGen α := do
-      if h : i < generalizations.size then
-        let (ty, pf) := generalizations[i]
-        let ty := (← instantiateMVars (ty.replace proofToFVar.get?)).cleanupAnnotations
-        withLocalDeclD (← mkFreshUserName `pf) ty fun fvar => do
-          go (i + 1) (fvars := fvars.push fvar) (pfs := pfs.push pf)
-            (proofToFVar := proofToFVar.insert pf fvar)
-            (propToFVar := propToFVar.insert ty fvar)
-      else
-        withNewLocalInstances fvars 0 do
-          let e' := e.replace proofToFVar.get?
-          modify fun s => { s with propToFVar }
-          k fvars pfs e'
-  go 0 #[] #[] (proofToFVar := {}) (propToFVar := propToFVar)
-
-partial def generalizeProofsCore'
-    (g : MVarId) (fvars rfvars : Array FVarId) (target : Bool) :
-    MGen (Array Expr × MVarId) := go g 0 #[]
-where
-  go (g : MVarId) (i : Nat) (hs : Array Expr) : MGen (Array Expr × MVarId) := g.withContext do
-    let tag ← g.getTag
-    if h : i < rfvars.size then
-      let fvar := rfvars[i]
-      if fvars.contains fvar then
-        let tgt ← instantiateMVars <| ← g.getType
-        let ty := (if tgt.isLet then tgt.letType! else tgt.bindingDomain!).cleanupAnnotations
-        if ← pure tgt.isLet <&&> Meta.isProp ty then
-          let tgt' := Expr.forallE tgt.letName! ty tgt.letBody! .default
-          let g' ← mkFreshExprSyntheticOpaqueMVar tgt' tag
-          g.assign <| .app g' tgt.letValue!
-          return ← go g'.mvarId! i hs
-        if let some pf := (← get).propToFVar.get? ty then
-          let tgt' := tgt.bindingBody!.instantiate1 pf
-          let g' ← mkFreshExprSyntheticOpaqueMVar tgt' tag
-          g.assign <| .lam tgt.bindingName! tgt.bindingDomain! g' tgt.bindingInfo!
-          return ← go g'.mvarId! (i + 1) hs
-        match tgt with
-        | .forallE n t b bi =>
-          let prop ← Meta.isProp t
-          withGeneralizedProofs' t none fun hs' pfs' t' => do
-            let t' := t'.cleanupAnnotations
-            let tgt' := Expr.forallE n t' b bi
-            let g' ← mkFreshExprSyntheticOpaqueMVar tgt' tag
-            g.assign <| mkAppN (← mkLambdaFVars hs' g' (usedOnly := false) (usedLetOnly := false)) pfs'
-            let (fvar', g') ← g'.mvarId!.intro1P
-            g'.withContext do Elab.pushInfoLeaf <|
-              .ofFVarAliasInfo { id := fvar', baseId := fvar, userName := ← fvar'.getUserName }
-            if prop then
-              MGen.insertFVar t' (.fvar fvar')
-            go g' (i + 1) (hs ++ hs')
-        | .letE n t v b _ =>
-          withGeneralizedProofs' t none fun hs' pfs' t' => do
-            withGeneralizedProofs' v t' fun hs'' pfs'' v' => do
-              let tgt' := Expr.letE n t' v' b false
-              let g' ← mkFreshExprSyntheticOpaqueMVar tgt' tag
-              g.assign <| mkAppN (← mkLambdaFVars (hs' ++ hs'') g' (usedOnly := false) (usedLetOnly := false)) (pfs' ++ pfs'')
-              let (fvar', g') ← g'.mvarId!.intro1P
-              g'.withContext do Elab.pushInfoLeaf <|
-                .ofFVarAliasInfo { id := fvar', baseId := fvar, userName := ← fvar'.getUserName }
-              go g' (i + 1) (hs ++ hs' ++ hs'')
-        | _ => unreachable!
-      else
-        let (fvar', g') ← g.intro1P
-        g'.withContext do Elab.pushInfoLeaf <|
-          .ofFVarAliasInfo { id := fvar', baseId := fvar, userName := ← fvar'.getUserName }
-        go g' (i + 1) hs
-    else if target then
-      withGeneralizedProofs' (← g.getType) none fun hs' pfs' ty' => do
-        let g' ← mkFreshExprSyntheticOpaqueMVar ty' tag
-        g.assign <| mkAppN (← mkLambdaFVars hs' g' (usedOnly := false) (usedLetOnly := false)) pfs'
-        return (hs ++ hs', g'.mvarId!)
-    else
-      return (hs, g)
-
-end GeneralizeProofs
-
-open Lean Elab Parser.Tactic Elab.Tactic Batteries.Tactic.GeneralizeProofs
-partial def generalizeProofs'
-    (g : MVarId) (fvars : Array FVarId) (target : Bool) (config : Config := {}) :
-    MetaM (Array Expr × MVarId) := do
-  let (rfvars, g) ← g.revert fvars (clearAuxDeclsInsteadOfRevert := true)
-  g.withContext do
-    let s := { propToFVar := ← initialPropToFVar }
-    GeneralizeProofs.generalizeProofsCore' g fvars rfvars target |>.run config |>.run' s
-
-elab (name := generalizeProofsElab'') "generalize_proofs" config?:(Parser.Tactic.config)?
-    hs:(ppSpace colGt binderIdent)* loc?:(location)? : tactic => withMainContext do
-  let config ← elabConfig (mkOptionalNode config?)
-  let (fvars, target) ←
-    match expandOptLocation (Lean.mkOptionalNode loc?) with
-    | .wildcard => pure ((← getLCtx).getFVarIds, true)
-    | .targets t target => pure (← getFVarIds t, target)
-  liftMetaTactic1 fun g => do
-    let (pfs, g) ← generalizeProofs' g fvars target config
-    g.withContext do
-      let mut lctx ← getLCtx
-      for h in hs, fvar in pfs do
-        if let `(binderIdent| $s:ident) := h then
-          lctx := lctx.setUserName fvar.fvarId! s.getId
-        Expr.addLocalVarInfoForBinderIdent fvar h
-      Meta.withLCtx lctx (← Meta.getLocalInstances) do
-        let g' ← Meta.mkFreshExprSyntheticOpaqueMVar (← g.getType) (← g.getTag)
-        g.assign g'
-        return g'.mvarId!
-
-end Harmonic
-
-lemma contdiffpol (n : ℕ) (p : Polynomial ℝ)
-    : ContDiffOn ℝ n p.eval (Ioo a b) := by
-  -- Since polynomials are infinitely differentiable, their nth derivative is also a polynomial, which is smooth. Therefore, the nth derivative is continuous on the interval.
-  have h_poly_cont_diff : ContDiffOn ℝ ⊤ (fun x => p.eval x) (Set.Ioo a b) := by
-    -- Since polynomials are infinitely differentiable, their evaluations are also infinitely differentiable.
-    have h_poly_cont_diff : ∀ p : Polynomial ℝ, ContDiffOn ℝ ⊤ (fun x => p.eval x) (Set.Ioo a b) := by
-      -- Since polynomials are infinitely differentiable, their evaluations are also infinitely differentiable. We can use the fact that the evaluation of a polynomial is a polynomial function, which is known to be infinitely differentiable.
-      intro p
-      have h_poly_cont_diff : ContDiff ℝ ⊤ (fun x => p.eval x) := by
-        -- Since polynomials are infinitely differentiable, their evaluations are also infinitely differentiable. Therefore, the function p.eval is ContDiff ℝ ⊤.
-        have h_poly_cont_diff : ∀ p : Polynomial ℝ, ContDiff ℝ ⊤ (fun x => p.eval x) := by
-          -- Since polynomials are analytic, we can apply the theorem that states that analytic functions are ContDiffOn.
-          have h_poly_analytic : ∀ p : Polynomial ℝ, AnalyticOn ℝ (fun x => p.eval x) Set.univ := by
-            intro p
-            have h_poly_analytic : AnalyticOn ℝ (fun x => p.eval x) Set.univ := by
-              have h_poly_analytic : ∀ x : ℝ, AnalyticAt ℝ (fun x => p.eval x) x := by
-                intro x
-                have h_poly_analytic : AnalyticAt ℝ (fun x => ∑ i ∈ Finset.range (p.natDegree + 1), p.coeff i * x ^ i) x := by
-                  fun_prop
-                generalize_proofs at *;
-                simpa only [ Polynomial.eval_eq_sum_range ] using h_poly_analytic
-              exact?
-            exact h_poly_analytic;
-          exact fun p => by simpa using h_poly_analytic p |> AnalyticOn.contDiff;
-        exact h_poly_cont_diff p
-        skip
-      exact h_poly_cont_diff.contDiffOn;
-    exact h_poly_cont_diff p;
-  -- Since ⊤ is greater than or equal to any natural number n, if the function is ContDiffOn ℝ ⊤, then it's also ContDiffOn ℝ n.
-  apply ContDiffOn.of_le; exact h_poly_cont_diff; exact le_top
-
+open Set Fin Filter Function
+open scoped NNReal Topology ContDiff
 
 theorem divided_difference_eq_deriv_n (n : ℕ) (hn_ne_0 : n ≠ 0) (nodes : Finset ℝ)
     (hab : a < b)
@@ -395,8 +162,10 @@ theorem divided_difference_eq_deriv_n (n : ℕ) (hn_ne_0 : n ≠ 0) (nodes : Fin
   have hE : ContDiffOn ℝ n (f - p.eval) (Ioo a b) := by
     apply ContDiffOn.sub hf hp
 
+  have nm1len : ↑n - 1 ≤ (n : WithTop ℕ∞):= by norm_num
+
   have hE' : ContDiffOn ℝ (n-1) (f - p.eval) (Ioo a b) := by
-    sorry
+    apply ContDiffOn.of_le hE nm1len
 
   have exists_c : ∃ c ∈ intOfHull nodes, iteratedDeriv n E c = 0 := by
     apply order_n_Rolle_F n hn_ne_0 nodes hcard hab nodesinab hEc hE' zeroE
@@ -408,8 +177,19 @@ theorem divided_difference_eq_deriv_n (n : ℕ) (hn_ne_0 : n ≠ 0) (nodes : Fin
 
   let p_deriv_n := iteratedDeriv n p.eval
 
-  have contdiff_f : ContDiffAt ℝ n f c := by
+  have c_in_ab : c ∈ (Ioo a b) := by
+    unfold intOfHull at hc1
+
     sorry
+
+  have contdiff_f : ContDiffAt ℝ n f c := by
+    have isopen : IsOpen (Ioo a b) := by
+      exact isOpen_Ioo
+
+    have ioo_is_neigh : (Ioo a b) ∈ 𝓝 c := by
+      exact IsOpen.mem_nhds isopen c_in_ab
+
+    exact ContDiffOn.contDiffAt hf ioo_is_neigh
 
   have contdiff_p : ContDiffAt ℝ n p.eval c := by
     sorry
